e4c972d467e90707cdfcd500c0b4709eacc30660
Kevin W <kjoewill@gmail.com>
2018-08-21 17:31:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ katz_deli = []
 def line(line)
   puts "The line is currently empty." if line.size == 0
   
-  put "The line is currently: "
+  print "The line is currently: "
   
-  line.each_with_index { |e,i| put " #{i}. #{e}" } 
+  line.each_with_index { |e,i| print " #{i}. #{e}" } 
   
   
 end
